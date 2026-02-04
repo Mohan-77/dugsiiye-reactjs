49a500daa57c3f76d70294b5c22e5660cffcac1d
mohamed <adeabdiomar>
2026-02-04 12:55:57 +0530

    Added exercise-17

---
+++ exercise-17/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import ContactForm from './ContactForm'
+
+function App() {
+  
+
+  return (
+    <>
+      <ContactForm />
+    </>
+  )
+}
+
+export default App
